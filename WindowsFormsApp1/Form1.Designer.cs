3cb6639f043775471328e9c9e3bf4bdec21e563e
Svistunova Katya <svistuniva05@mail.ru>
2026-01-17 10:23:48 +0300

    Добавлен обработчик кнопки

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -48,6 +48,7 @@
             this.textBox8 = new System.Windows.Forms.TextBox();
             this.textBox9 = new System.Windows.Forms.TextBox();
             this.textBox10 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -210,11 +211,22 @@
             this.textBox10.Size = new System.Drawing.Size(100, 22);
             this.textBox10.TabIndex = 19;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(510, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 50);
+            this.button1.TabIndex = 20;
+            this.button1.Text = "Создать документ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox10);
             this.Controls.Add(this.textBox9);
             this.Controls.Add(this.textBox8);
@@ -264,6 +276,7 @@
         private System.Windows.Forms.TextBox textBox8;
         private System.Windows.Forms.TextBox textBox9;
         private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace WindowsFormsApp1
 {
@@ -15,6 +9,51 @@ namespace WindowsFormsApp1
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string templatePath = @"C:\Users\svist\Desktop\институт\3 курс\5 семестр\КРПП\WindowsFormsApp1\template.docx";
+            string savePath = @"C:\Users\svist\Desktop\институт\3 курс\5 семестр\КРПП\WindowsFormsApp1\Gotoviy_Akt.docx";
+
+            Word.Application wordApp = new Word.Application();
+            Word.Document doc = null;
+
+            try
+            {
+                doc = wordApp.Documents.Open(templatePath);
+
+                ReplaceStub("{Метка1}", textBox1.Text, doc);
+                ReplaceStub("{Метка2}", textBox2.Text, doc);
+                ReplaceStub("{Метка3}", textBox3.Text, doc);
+                ReplaceStub("{Метка4}", textBox4.Text, doc);
+                ReplaceStub("{Метка5}", textBox5.Text, doc);
+                ReplaceStub("{Метка6}", textBox6.Text, doc);
+                ReplaceStub("{Метка7}", textBox7.Text, doc);
+                ReplaceStub("{Метка8}", textBox8.Text, doc);
+                ReplaceStub("{Метка9}", textBox9.Text, doc);
+                ReplaceStub("{Метка10}", textBox10.Text, doc);
+
+                doc.SaveAs2(savePath);
+
+                MessageBox.Show("Документ успешно создан по адресу: " + savePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message);
+            }
+            finally
+            {
+                if (doc != null) doc.Close();
+                wordApp.Quit();
+            }
+
+        }
+        private void ReplaceStub(string stub, string text, Word.Document doc)
+        {
+            var range = doc.Content;
+            range.Find.ClearFormatting();
+            range.Find.Execute(FindText: stub, ReplaceWith: text);
         }
     }
 }
